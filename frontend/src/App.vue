944b937fd4cf8ce55a84db9a9bbecad9924982a2
unknown <joan.ferrero.montiel@gmail.com>
2023-01-22 21:51:27 +0100

    finish Project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <h1>Hola</h1>
+  <Navbar />
+  <main class="container py-5">
+    <RouterView />
+  </main>
 </template>
+
+<script lang="ts">
+  import { defineComponent } from "@vue/runtime-core";
+  import Navbar from "./components/Navbar.vue";
+  import { RouterView } from 'vue-router'
+
+  export default defineComponent({
+    name: "App",
+    components: {
+      Navbar,
+    },
+  });
+</script>
 
 <style scoped>
 </style>
