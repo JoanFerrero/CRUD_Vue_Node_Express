0b4107b40c28108d7f337992ab1dc4d0bee7c796
Joan Ferrero <102603517+JoanFerrero@users.noreply.github.com>
2023-01-22 21:53:25 +0100

    Delete database.ts

--- backend/database.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from "mongoose";
-
-export const startConnection = async (): Promise<void> => {
-  try {
-    await connect("mongodb+srv://joan:1234joan1234@cluster0.6swopdo.mongodb.net/?retryWrites=true&w=majority");
-  } catch (error) {
-    console.error(error);
-  }
-};
